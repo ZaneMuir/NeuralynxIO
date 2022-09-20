d9afda30818fe076d5913c8e9e4d2d5b8f5ad544
Yizhan Miao <yizhanmiao@pm.me>
2022-09-19 23:49:51 -0700

    fix: typo

--- src/io_nev.jl
+++ src/io_nev.jl
@@ -6,16 +6,16 @@
 end
 
 function load_neuralynx_nev(filename::AbstractString; mode::LOAD_MODE_NEV=LOAD_MODE_NEV_RAW, bit_event::Integer=0, byte_event::Integer=0, string_event::String="")
-    raw = NRDFile(EventBlock, filename)
+    raw = NRDFile{EventBlock}(filename)
     if mode == LOAD_MODE_NEV_RAW
         raw
     elseif mode == LOAD_MODE_NEV_BIT_TIMESTAMPS
         @assert 1 <= bit_event <= 8 "ttl only has 8 pins." #TODO: check if it is true.
         _bit_mask = 1 << (bit_event-1)
-        map(xx->xx.qwTimeStamp, filter(x->(x.ttl & _bit_mask) == _bit_mask, raw.data))
+        map(xx->xx.qwTimeStamp, filter(x->(x.nttl & _bit_mask) == _bit_mask, raw.data))
     elseif mode == LOAD_MODE_NEV_BYTE_TIMESTAMPS
         @assert 0 <= byte_event <= typemax(UInt8) "ttl only has 8 pins."
-        map(xx->xx.qwTimeStamp, filter(x->x.ttl == byte_event, raw.data))
+        map(xx->xx.qwTimeStamp, filter(x->x.nttl == byte_event, raw.data))
     elseif mode == LOAD_MODE_NEV_STRING_TIMESTAMPS
         map(xx->xx.qwTimeStamp, filter(x->strip(String(x.EventString), '\0') == string_event, raw.data))
     end
